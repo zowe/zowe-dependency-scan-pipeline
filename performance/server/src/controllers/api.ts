ed193d05a762a4c4a7ea2fbbefa472f51211384b
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2020-01-10 09:44:33 -0500

    remove uneccessary files
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- performance/server/src/controllers/api.ts
+++ performance/server/src/controllers/api.ts
@@ -1,8 +1,6 @@
 "use strict";
 
-import graph from "fbgraph";
 import { Response, Request, NextFunction } from "express";
-import { UserDocument } from "../models/User";
 
 
 /**
@@ -12,22 +10,5 @@ import { UserDocument } from "../models/User";
 export const getApi = (req: Request, res: Response) => {
     res.render("api/index", {
         title: "API Examples"
-    });
-};
-
-/**
- * GET /api/facebook
- * Facebook API example.
- */
-export const getFacebook = (req: Request, res: Response, next: NextFunction) => {
-    const user = req.user as UserDocument;
-    const token = user.tokens.find((token: any) => token.kind === "facebook");
-    graph.setAccessToken(token.accessToken);
-    graph.get(`${user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
-        if (err) { return next(err); }
-        res.render("api/facebook", {
-            title: "Facebook API",
-            profile: results
-        });
     });
 };
