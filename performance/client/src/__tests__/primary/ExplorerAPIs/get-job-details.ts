307a69cd7d7b1c377017b162f7cc0941cb6a27a2
CaretCake <caretcake@outlook.com>
2020-11-11 10:49:11 -0500

    Started modifying job details testcase to dynamically get job id
    Signed-off-by: CaretCake <caretcake@outlook.com>

--- performance/client/src/__tests__/primary/ExplorerAPIs/get-job-details.ts
+++ performance/client/src/__tests__/primary/ExplorerAPIs/get-job-details.ts
@@ -8,22 +8,49 @@
  * Copyright IBM Corporation 2020
  */
 
+import got from "got";
 import WrkTestCase from "../../../testcase/wrk";
 import { getBasicAuthorizationHeader } from "../../../utils";
 
 class ExplorerApiJobDetailsTest extends WrkTestCase {
   fetchZoweVersions = true;
-
   name = "Test explorer data sets api endpoint /api/v2/jobs/{jobName}/{jobId}";
   endpoint = '/api/v2/jobs/SDSF/STC01060';
 
-  duration = 15 * 60;
+  duration = 5;
   concurrency = 10;
   threads = 1;
+  debug = true;
 
   async before(): Promise<void> {
     await super.before();
+
+    try{
+      const url = `https://${this.targetHost}:${this.targetPort}/api/v2/jobs?prefix=SDSF*&status=ACTIVE`;
+      console.log("calling: " + url);
+    
+      //let authHeader = getBasicAuthorizationHeader();
+
+      let user = process.env.TEST_AUTH_USER;
+      let password = process.env.TEST_AUTH_PASSWORD;
+
+      console.log("calling: " + url);
+      const response =  await got(url, {
+        https: {
+          rejectUnauthorized: false
+        },
+        headers: {
+          "Authorization":Buffer.from(`${user}:${password}`).toString("base64")
+        }
+      });
+      console.log("res: " + response.body);
+    } catch (e) {
+      console.log('e: ' + e);
+      console.log('error: ' + e.response.body);
+    }
+   
     this.headers.push(getBasicAuthorizationHeader());
+ 
   }
 }
 
