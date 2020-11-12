c5a60e8bb1919d6cf288389b27f1c605f0767d83
CaretCake <caretcake@outlook.com>
2020-11-11 19:41:23 -0500

    Modified test case to get SDSF job ID dynamically
    Signed-off-by: CaretCake <caretcake@outlook.com>

--- performance/client/src/__tests__/primary/ExplorerAPIs/get-job-output-file-content.ts
+++ performance/client/src/__tests__/primary/ExplorerAPIs/get-job-output-file-content.ts
@@ -8,21 +8,46 @@
  * Copyright IBM Corporation 2020
  */
 
+import got from "got";
 import WrkTestCase from "../../../testcase/wrk";
-import { getBasicAuthorizationHeader } from "../../../utils";
+import { getBasicAuthorizationHeader, getBasicAuthorizationHeaderValue } from "../../../utils";
+import PerformanceTestException from "../../../exceptions/performance-test-exception";
 
 class ExplorerApiJobOutputFileContentTest extends WrkTestCase {
   fetchZoweVersions = true;
 
   name = "Test explorer data sets api endpoint /api/v2/jobs/{jobName}/{jobId}/files/{fileId}/content";
-  endpoint = '/api/v2/jobs/SDSF/STC01060/files/3/content';
+  endpoint = '/api/v2/jobs/SDSF/{jobId}/files/{fileId}/content';
 
-  duration = 15 * 60;
+  duration = 5;
   concurrency = 10;
   threads = 1;
 
   async before(): Promise<void> {
     await super.before();
+
+    // get active job ID
+    const jobUrl = `https://${this.targetHost}:${this.targetPort}/api/v2/jobs?prefix=SDSF&status=ACTIVE&owner=*`;
+    const { body } =  await got(jobUrl, {
+      https: {
+        rejectUnauthorized: false
+      },
+      headers: {
+        "Authorization": getBasicAuthorizationHeaderValue()
+      },
+      responseType: 'json'
+    }); 
+    const jobs = body as {items: [{[key: string]: string|null}]};
+    const jobId = jobs && jobs.items[0] && jobs.items[0]['jobId'];
+    if (!jobId) {
+      throw new PerformanceTestException("Cannot find job ID for testing");
+    }
+
+    // apply the changes to endpoint and test url
+    this.endpoint = this.endpoint.replace('{jobId}', jobId);
+    this.endpoint = this.endpoint.replace('{fileId}', '3');
+    this.fullUrl = `https://${this.targetHost}:${this.targetPort}${this.endpoint}`;
+
     this.headers.push(getBasicAuthorizationHeader());
   }
 }
