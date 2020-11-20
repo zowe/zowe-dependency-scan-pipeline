e0842d2ea378ea1e29dc0966f6a7a0a096f427d6
CaretCake <caretcake@outlook.com>
2020-11-20 14:22:16 -0500

    Added desktop unix file content endpoint test and desktop auth cookie header function
    Signed-off-by: CaretCake <caretcake@outlook.com>

--- performance/client/src/utils/zowe.ts
+++ performance/client/src/utils/zowe.ts
@@ -9,7 +9,9 @@
  */
 
 import got from "got";
+import { Cookie } from "tough-cookie";
 import { getBasicAuthorizationHeaderValue } from "../utils";
+import PerformanceTestException from "../exceptions/performance-test-exception";
 
 /**
  * Fetch Zowe instance version from APIML Gateway
@@ -41,12 +43,14 @@ export const getZoweVersions = async (apimlGatewayHost: string, apimlGatewayPort
 /**
  * Get the jobID of a job
  *
+ * @param apimlGatewayHost
+ * @param apimlGatewayPort
  * @param jobName
  * @param jobStatus
  * @param jobOwner
  */
-export const getJobId = async (targetHost: string, targetPort: number, jobName: string, jobStatus: string, jobOwner: string): Promise<string> => {
-  const url = `https://${targetHost}:${targetPort}/api/v2/jobs?prefix=${jobName}&status=${jobStatus}&owner=${jobOwner}`;
+export const getJobId = async (apimlGatewayHost: string, apimlGatewayPort: number, jobName: string, jobStatus: string, jobOwner: string): Promise<string> => {
+  const url = `https://${apimlGatewayHost}:${apimlGatewayPort}/api/v2/jobs?prefix=${jobName}&status=${jobStatus}&owner=${jobOwner}`;
 
   const { body } =  await got(url, {
     https: {
@@ -64,13 +68,15 @@ export const getJobId = async (targetHost: string, targetPort: number, jobName: 
 };
 
 /**
- * Get the the fileID of first output file of a job
+ * Get the fileID of first output file of a job
  *
+ * @param apimlGatewayHost
+ * @param apimlGatewayPort
  * @param jobName
  * @param jobId
  */
-export const getFileId = async (targetHost: string, targetPort: number, jobName: string, jobId: string): Promise<string> => {
-  const url = `https://${targetHost}:${targetPort}/api/v2/jobs/${jobName}/${jobId}/files`;
+export const getFileId = async (apimlGatewayHost: string, apimlGatewayPort: number, jobName: string, jobId: string): Promise<string> => {
+  const url = `https://${apimlGatewayHost}:${apimlGatewayPort}/api/v2/jobs/${jobName}/${jobId}/files`;
 
   const { body } =  await got(url, {
     https: {
@@ -85,4 +91,53 @@ export const getFileId = async (targetHost: string, targetPort: number, jobName:
   const fileId = files && files.items[0] && files.items[0]['id'];
 
   return fileId;
+};
+
+/**
+ * Get cookie from Desktop auth endpoint
+ *
+ * @param apimlGatewayHost
+ * @param apimlGatewayPort
+ * @param user
+ * @param password
+ */
+export const getDesktopAuthenticationCookieHeader =  async (apimlGatewayHost: string, apimlGatewayPort: number, user?: string, password?: string): Promise<string> => {
+  const url = `https://${apimlGatewayHost}:${apimlGatewayPort}/ui/v1/zlux/auth`;
+
+  if (!user) {
+    user = process.env.TEST_AUTH_USER;
+  }
+  if (!password) {
+    password = process.env.TEST_AUTH_PASSWORD;
+  }
+  if (!user || !password) {
+    throw new PerformanceTestException("Username and password are required to login desktop");
+  }
+
+  const { statusCode, headers, body } =  await got.post(url, {
+    https: {
+      rejectUnauthorized: false
+    },
+    json: {
+      username: user,
+      password,
+    }
+  });
+
+  if (statusCode !== 200) {
+    throw new PerformanceTestException(`Authentication failed with desktop, status code is ${statusCode}`);
+  }
+
+  let cookies: Cookie[] = [];
+  if (Array.isArray(headers['set-cookie'])) {
+    cookies = headers['set-cookie'].map((cookieString) => {
+      return Cookie.parse(cookieString);
+    });
+  } else {
+    cookies = [Cookie.parse(`${headers['set-cookie']}`)];
+  }
+
+  return 'Cookie: ' + cookies.map((cookie): string => {
+    return cookie.key + '=' + cookie.value;
+  }).join('; ');
 };
