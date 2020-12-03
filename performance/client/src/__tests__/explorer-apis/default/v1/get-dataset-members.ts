47a091fd055e7df42cd1a9bdc84bf698c00edc4d
CaretCake <caretcake@outlook.com>
2020-12-03 16:05:21 -0500

    Created v1 explorer-api test cases
    Signed-off-by: CaretCake <caretcake@outlook.com>
    Signed-off-by: Melissa Farrell <melissa.farrell@ibm.com@Melissas-MBP.attlocal.net>

---
+++ performance/client/src/__tests__/explorer-apis/default/v1/get-dataset-members.ts
@@ -0,0 +1,30 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright IBM Corporation 2020
+ */
+
+import WrkTestCase from "../../../../testcase/wrk";
+import { getBasicAuthorizationHeader } from "../../../../utils";
+
+class ExplorerApiDatasetMemberListTest extends WrkTestCase {
+  fetchZoweVersions = true;
+
+  name = "Test explorer api endpoint /api/v1/datasets/{ds}/members";
+  endpoint = '/api/v1/datasets/SYS1.HELP/members';
+
+  duration = 15 * 60;
+  concurrency = 10;
+  threads = 1;
+
+  async before(): Promise<void> {
+    await super.before();
+    this.headers.push(getBasicAuthorizationHeader());
+  }
+}
+
+new ExplorerApiDatasetMemberListTest().init();
