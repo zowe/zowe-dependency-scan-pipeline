67640059ff269c6b7d951d22d1a8ad08fb727ff3
CaretCake <caretcake@outlook.com>
2020-12-03 13:36:39 -0500

    Created test case for job output files contents endpoint
    Signed-off-by: CaretCake <caretcake@outlook.com>
    Signed-off-by: Melissa Farrell <melissa.farrell@ibm.com@Melissas-MBP.attlocal.net>

---
+++ performance/client/src/__tests__/explorer-apis/default/v2/get-job-output-files-contents.ts
@@ -0,0 +1,43 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright IBM Corporation 2020
+ */
+
+import WrkTestCase from "../../../../testcase/wrk";
+import { getBasicAuthorizationHeader } from "../../../../utils";
+import { getJobId } from "../../../../utils/zowe";
+import PerformanceTestException from "../../../../exceptions/performance-test-exception";
+
+class ExplorerApiJobOutputFilesContentsTest extends WrkTestCase {
+  fetchZoweVersions = true;
+
+  name = "Test explorer api endpoint /api/v2/jobs/{jobName}/{jobId}/files/content";
+  endpoint = '/api/v2/jobs/SDSF/{jobId}/files/content';
+
+  duration = 15 * 60;
+  concurrency = 10;
+  threads = 1;
+
+  async before(): Promise<void> {
+    await super.before();
+
+    // get active job ID
+    const jobId = await getJobId(this.targetHost, this.targetPort, 'SDSF', 'ACTIVE', '*');
+    if (!jobId) {
+      throw new PerformanceTestException("Cannot find job ID for testing");
+    }
+
+    // apply the changes to endpoint and test url
+    this.endpoint = this.endpoint.replace('{jobId}', jobId);
+    this.fullUrl = `https://${this.targetHost}:${this.targetPort}${this.endpoint}`;
+
+    this.headers.push(getBasicAuthorizationHeader());
+  }
+}
+
+new ExplorerApiJobOutputFilesContentsTest().init();
