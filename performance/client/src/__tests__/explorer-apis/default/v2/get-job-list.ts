332181e2404fe0b3729c9d290e2c0164b010f328
CaretCake <caretcake@outlook.com>
2020-12-03 14:21:02 -0500

    Added primary test suite tests to explorer-apis component test suite
    Signed-off-by: CaretCake <caretcake@outlook.com>
    Signed-off-by: Melissa Farrell <melissa.farrell@ibm.com@Melissas-MBP.attlocal.net>

---
+++ performance/client/src/__tests__/explorer-apis/default/v2/get-job-list.ts
@@ -0,0 +1,30 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright IBM Corporation 2020
+ */
+
+import WrkTestCase from "../../../../testcase/wrk";
+import { getBasicAuthorizationHeader } from "../../../../utils";
+
+class ExplorerApiJobListTest extends WrkTestCase {
+  fetchZoweVersions = true;
+
+  name = "Test explorer api endpoint /api/v2/jobs";
+  endpoint = '/api/v2/jobs?prefix=SDSF&status=ACTIVE&owner=*';
+
+  duration = 15 * 60;
+  concurrency = 10;
+  threads = 1;
+
+  async before(): Promise<void> {
+    await super.before();
+    this.headers.push(getBasicAuthorizationHeader());
+  }
+}
+
+new ExplorerApiJobListTest().init();
