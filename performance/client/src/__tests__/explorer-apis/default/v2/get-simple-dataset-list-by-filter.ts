cc20d43f8cc8dc5320311e40ca6c99f9a3acd0b8
CaretCake <caretcake@outlook.com>
2020-12-03 13:47:26 -0500

    Created test case for simple dataset list endpoint
    Signed-off-by: CaretCake <caretcake@outlook.com>
    Signed-off-by: Melissa Farrell <melissa.farrell@ibm.com@Melissas-MBP.attlocal.net>

---
+++ performance/client/src/__tests__/explorer-apis/default/v2/get-simple-dataset-list-by-filter.ts
@@ -0,0 +1,30 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright IBM Corporation 2020
+ */
+
+import WrkTestCase from "../../../../testcase/wrk";
+import { getBasicAuthorizationHeader } from "../../../../utils";
+
+class ExplorerApiDatasetSimpleListByFilterTest extends WrkTestCase {
+  fetchZoweVersions = true;
+
+  name = "Test explorer api endpoint /api/v2/datasets/{filter}/list";
+  endpoint = '/api/v2/datasets/SYS1.HELP/list';
+
+  duration = 5;
+  concurrency = 10;
+  threads = 1;
+
+  async before(): Promise<void> {
+    await super.before();
+    this.headers.push(getBasicAuthorizationHeader());
+  }
+}
+
+new ExplorerApiDatasetSimpleListByFilterTest().init();
