c3a46e75aac968e278ce394fba0bf0a3d207d27b
Jack (T.) Jia <jack-tiefeng.jia@ibm.com>
2021-01-04 18:06:50 -0500

    purge jobs every day
    Signed-off-by: Jack (T.) Jia <jack-tiefeng.jia@ibm.com>

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -63,5 +63,19 @@ echo "rm -fr tomcat.*" | su
 echo
 
 ################################################################################
+echo "[${SCRIPT_NAME}] purge job queue and output"
+opercmd=/ZOWE/zowe-installs/opercmd.rexx
+if [ -f "${opercmd}" ]; then
+  echo "[${SCRIPT_NAME}] - \$P JQ,DAYS>2,PROT"
+  "${opercmd}" '$P JQ,DAYS>2,PROT'
+  echo "[${SCRIPT_NAME}] - \$PO STC1-9999"
+  "${opercmd}" '$PO STC1-9999'
+  echo "[${SCRIPT_NAME}] - \$PO TSU1-9999"
+  "${opercmd}" '$PO TSU1-9999'
+  echo "[${SCRIPT_NAME}] - \$PO JOB1-9999"
+  "${opercmd}" '$PO JOB1-9999'
+fi
+
+################################################################################
 echo "[${SCRIPT_NAME}] done."
 exit 0
