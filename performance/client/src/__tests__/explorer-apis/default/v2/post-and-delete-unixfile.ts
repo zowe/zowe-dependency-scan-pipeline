a1b8b93f26fec974412c7efeb08ffa928a8afb43
CaretCake <caretcake@outlook.com>
2021-01-11 14:19:22 -0500

    Created sequential test case for post and delete unix file endpoints
    Signed-off-by: CaretCake <caretcake@outlook.com>
    Signed-off-by: Melissa Farrell <melissa.farrell@ibm.com@Melissas-MBP.attlocal.net>

---
+++ performance/client/src/__tests__/explorer-apis/default/v2/post-and-delete-unixfile.ts
@@ -0,0 +1,51 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright IBM Corporation 2020
+ */
+
+import WrkSequentialEndpointsTestCase from "../../../../testcase/wrk-sequential-endpoints";
+import { SequentialHttpRequest } from "../../../../types";
+import { getBasicAuthorizationHeader } from "../../../../utils";
+
+class ExplorerApiPostAndDeleteUnixFileTest extends WrkSequentialEndpointsTestCase {
+  fetchZoweVersions = true;
+
+  name = "Test explorer api endpoints POST /api/v2/unixfiles/{path} and DELETE /api/v2/unixfiles/{path}"; 
+
+  endpoints = [
+    {
+      endpoint : "/api/v2/unixfiles/tmp/zowe-performance-test-file",
+      method   : "POST",
+      body     : `{"type": "FILE"}`,
+      headers  : [
+        "Content-Type: application/json",
+      ],
+      sequence : 0,
+    },
+    {
+      delay    : [100, 100],
+      endpoint : "/api/v2/unixfiles/tmp/zowe-performance-test-file",
+      method   : "DELETE",
+      sequence : 10,
+    },
+  ] as SequentialHttpRequest[];
+
+  duration = 1;
+  cooldown = 0;
+  concurrency = 1;
+  threads = 1;
+  debug = true;
+
+  async before(): Promise<void> {
+    await super.before();
+
+    this.headers.push(getBasicAuthorizationHeader());
+  }
+}
+
+new ExplorerApiPostAndDeleteUnixFileTest().init();
