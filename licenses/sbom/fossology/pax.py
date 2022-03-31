dcaa7e48a8dcccb9e343a0e4d7cee6e3cbfa13fa
MarkAckert <mark.ackert@broadcom.com>
2022-03-31 16:54:37 -0400

    cli, general local upload support
    Signed-off-by: MarkAckert <mark.ackert@broadcom.com>

--- licenses/sbom/fossology/pax.py
+++ licenses/sbom/fossology/pax.py
@@ -7,17 +7,18 @@ from fossology.obj import AccessLevel, ReportFormat
 
 foss_session = FossSession().session
 output_dir = os.getenv("OUTPUT_DIR", ".")
-
+upload_path = os.getenv("UPLOAD_DIR", "/tmp/upload")
+upload_name = os.getenv("UPLOAD_NAME", "default_up_name")
 server = {
-    "path": "/tmp/zowe_pax",
-    "name": "Zowe PAX",
+    "path": upload_path,
+    "name": upload_name,
 }
 
-print(f'Uploading Pax from mount point...', flush=True)
+print(f'Uploading File(s) from mount point...', flush=True)
 
-pax_upload = foss_session.upload_file(folder=foss_session.rootFolder,
+foss_upload = foss_session.upload_file(folder=foss_session.rootFolder,
                                       server=server,
-                                      description="Upload PAX from Server",
+                                      description="Upload File(s) from Server",
                                       access_level=AccessLevel.PUBLIC,
                                       wait_time=140)  # Retries 10 times, 140s x 10 = 23.33 minute total timeout
 
@@ -49,11 +50,11 @@ job_specification = {
         "reuse_copyright": True,
     },
 }
-pax_upload = foss_session.list_uploads(name="Zowe PAX")[0][0]
+foss_upload = foss_session.list_uploads(name=upload_name)[0][0]
 
-print('Scanning PAX...', flush=True)
+print('Scanning Upload...', flush=True)
 scan_res = foss_session.schedule_jobs(
-     foss_session.rootFolder, pax_upload, job_specification, wait=True,timeout=60)
+    foss_session.rootFolder, foss_upload, job_specification, wait=True, timeout=60)
 
 # Keep waiting on jobs while they are in progress. If any job ETA > 0, this keeps looping
 # This will wait (sleep) for jobs to complete based on their reported ETA
@@ -64,15 +65,19 @@ while allJobsDone == False:
     for job in all_jobs[0]:
         if job.eta > 0:
             allJobsDone = False
-            print(f'Waiting on {job.__str__()} to complete, will wait ETA of {job.eta}', flush=True)
+            print(
+                f'Waiting on {job.__str__()} to complete, will wait ETA of {job.eta}', flush=True)
             foss_session.detail_job(job.id, wait=True, timeout=job.eta)
 
 
 print('Generating report...', flush=True)
-pax_report = foss_session.generate_report(pax_upload, ReportFormat.SPDX2TV)
-report_content, report_name = foss_session.download_report(pax_report)
+spdx_report = foss_session.generate_report(foss_upload, ReportFormat.SPDX2TV)
+report_content, report_name = foss_session.download_report(spdx_report)
 
-print(f'Writing report to {const.OUTPUT_DIR}{os.path.sep}zowe-pax.spdx', flush=True)
-f = open(f'{const.OUTPUT_DIR}{os.path.sep}zowe-pax.spdx', "a")
+upload_filename = upload_name.replace(' ', '_')
+
+print(
+    f'Writing report to {const.OUTPUT_DIR}{os.path.sep}{upload_filename}.spdx', flush=True)
+f = open(f'{const.OUTPUT_DIR}{os.path.sep}{upload_filename}.spdx', "a")
 f.write(str(report_content, "utf-8"))
 f.close()
