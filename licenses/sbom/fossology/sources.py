98bcead3cb3b9d22d8771827098eeb8f7677f847
MarkAckert <mark.ackert@broadcom.com>
2022-04-01 17:06:42 -0400

    build more failure tolerance in
    Signed-off-by: MarkAckert <mark.ackert@broadcom.com>

--- licenses/sbom/fossology/sources.py
+++ licenses/sbom/fossology/sources.py
@@ -1,14 +1,18 @@
 import asyncio
 import json
+import os
 import pprint
 import random
 import string
+import sys
 from logging import exception
 from os import fsync, path
 from types import SimpleNamespace
 from typing import Any
 
 import requests
+from tenacity import TryAgain
+from tryagain import retries
 
 import const
 from errors import RequestError
@@ -30,23 +34,32 @@ class JobScan:
         self.job_spec = job_spec
 
 
+async def check_jobs_complete(upload):
+    allJobsComplete = False
+    while not allJobsComplete:
+        allJobsComplete = True
+        job_detail = foss_session.list_jobs(upload)
+        for job in job_detail[0]:
+            if (job.status == 'Queued' or job.status == "Starting") or job.eta > 0:
+                allJobsComplete = False
+                await asyncio.sleep(max(job.eta / 3, 10))
+
+
 async def scan_and_report_worker(name: string, queue: asyncio.Queue):
     while True:
+        scan_job: JobScan = await queue.get()
         try:
-            scan_job: JobScan = await queue.get()
-            print(f'Scanning {scan_job.upload.uploadname}')
+            print(f'Scanning {scan_job.upload.uploadname}', flush=True)
 
             _ = foss_session.schedule_jobs(scan_job.folder, scan_job.upload,
-                                           scan_job.job_spec, wait=True)
+                                           scan_job.job_spec, wait=False)
 
+            await check_jobs_complete(scan_job.upload)
 
-            foss_client.waitForUploadJobs(scan_job.upload)
-         
             report_id = foss_session.generate_report(
                 scan_job.upload, ReportFormat.SPDX2TV)
 
-         
-            foss_client.waitForUploadJobs(scan_job.upload)
+            await check_jobs_complete(scan_job.upload)
 
             report_content, _ = foss_session.download_report(report_id)
 
@@ -57,21 +70,45 @@ async def scan_and_report_worker(name: string, queue: asyncio.Queue):
             queue.task_done()
         except:
             print(
-                f'Exception in scan and report worker for {scan_job.upload.uploadname}')
+                f'Exception in scan and report worker for {scan_job.upload.uploadname}', flush=True)
             raise
 
 
 async def clone_worker(name: string, queue: asyncio.Queue):
     while True:
         vcs_root = await queue.get()
-        print(f'Cloning {vcs_root["vcsName"]}')
-        vcs_res = foss_session.upload_file(
-            foss_session.rootFolder,
-            vcs=vcs_root,
-            description=f'Upload {vcs_root["vcsName"]}',
-            access_level=AccessLevel.PUBLIC,
-        )
-        queue.task_done()
+
+        print(
+            f'Checking if {vcs_root["vcsName"]} already exists...', flush=True)
+        try:
+            uploads = foss_session.list_uploads(
+                foss_session.rootFolder, name=f'{vcs_root["vcsName"]}', all_pages=True)
+            uploadFound = False
+            for upload in uploads[0]:
+                if upload.uploadname == vcs_root["vcsName"]:
+                    uploadFound = True
+            if uploadFound:
+                print(f'Already exists: {vcs_root["vcsName"]}', flush=True)
+                queue.task_done()
+            else:
+                print(f'Cloning {vcs_root["vcsName"]}', flush=True)
+                try:
+                    vcs_res = foss_session.upload_file(
+                        foss_session.rootFolder,
+                        vcs=vcs_root,
+                        description=f'{vcs_root["vcsName"]}',
+                        access_level=AccessLevel.PUBLIC
+                    )
+                    await asyncio.sleep(5)
+                    queue.task_done()
+                except:
+                    sys.exit(
+                        f'Failed to clone {vcs_root["vcsName"]}, quitting...')
+
+        except:
+            # nothing
+            print('Existing upload not found.')
+            raise
 
 
 async def main():
@@ -90,41 +127,48 @@ async def main():
 
     foss_vcs_roots = []
 
-    for dependency in manifestJson.sourceDependencies:
-        for repository in dependency.entries:
-            foss_vcs_roots.append({
-                "vcsType": "git",
-                "vcsUrl": f'https://github.com/zowe/{repository.repository}',
-                "vcsName": f'{repository.repository}-{repository.tag}',
-                "vcsBranch": f'{repository.tag}',
-            })
+    try:
+        for dependency in manifestJson.sourceDependencies:
+            for repository in dependency.entries:
 
-    foss_clone_queue = asyncio.Queue(foss_vcs_roots.__len__())
-    foss_scan_queue = asyncio.Queue(foss_vcs_roots.__len__())
-    foss_tasks = []
+                sanitized_tag = repository.tag.replace('/', '_')
 
-    for i in range(FOSS_CLONE_TASK_COUNT):
-        asyncio.create_task(clone_worker(f'worker{i}', foss_clone_queue))
-    for i in range(FOSS_SCAN_TASK_COUNT):
-        asyncio.create_task(scan_and_report_worker(
-            f'scan_worker_{i}', foss_scan_queue))
+                foss_vcs_roots.append({
+                    "vcsType": "git",
+                    "vcsUrl": f'https://github.com/zowe/{repository.repository}',
+                    "vcsName": f'{repository.repository}-{sanitized_tag}',
+                    "vcsBranch": f'{repository.tag}',
+                })
 
-    # Temp hard-code
-    for vcsRoot in foss_vcs_roots[2:4]:
-        foss_clone_queue.put_nowait(vcsRoot)
+        foss_clone_queue = asyncio.Queue(foss_vcs_roots.__len__())
+        foss_scan_queue = asyncio.Queue(foss_vcs_roots.__len__())
+        foss_tasks = []
 
-    await foss_clone_queue.join()
+        for i in range(FOSS_CLONE_TASK_COUNT):
+            asyncio.create_task(clone_worker(f'worker{i}', foss_clone_queue))
+        for i in range(FOSS_SCAN_TASK_COUNT):
+            asyncio.create_task(scan_and_report_worker(
+                f'scan_worker_{i}', foss_scan_queue))
 
-    vcs_up, pages = foss_session.list_uploads()
+        # Temp hard-code
+        for vcsRoot in foss_vcs_roots:
+            # print('skip clone', flush=True)
+            foss_clone_queue.put_nowait(vcsRoot)
 
-    for upload in vcs_up:
+        await foss_clone_queue.join()
 
-        job_scan = JobScan(Folder(upload.folderid, upload.foldername, "", ""),
-                           upload,
-                           const.JOB_SPEC_NO_NOMOS)
+        vcs_up, pages = foss_session.list_uploads()
 
-        foss_scan_queue.put_nowait(job_scan)
+        for upload in vcs_up:
 
-    await foss_scan_queue.join()
+            job_scan = JobScan(Folder(upload.folderid, upload.foldername, "", ""),
+                               upload,
+                               const.JOB_SPEC_NO_NOMOS)
+
+            foss_scan_queue.put_nowait(job_scan)
+
+        await foss_scan_queue.join()
+    except:
+        raise TryAgain
 
 asyncio.run(main())
