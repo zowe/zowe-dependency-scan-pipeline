cb7d7bcf01d53892e6810913478f0e58e647c48f
Timothy Johnson <timothy.johnson@broadcom.com>
2026-09-23 15:38:26 -0400

    Add missing injectable decorator
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- licenses/dependency-scan/src/actions/ort/OrtBaseAction.ts
+++ licenses/dependency-scan/src/actions/ort/OrtBaseAction.ts
@@ -1,6 +1,9 @@
 import * as fs from "fs";
+import { injectable } from "inversify";
+import "reflect-metadata";
 import { Constants } from "../../constants/Constants";
 
+@injectable()
 export class OrtBaseAction {
     constructor() {
         if (!fs.existsSync(Constants.ORT_PROG_FILE)) {
